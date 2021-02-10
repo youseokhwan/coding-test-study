713621a773646b6ef65357ede957256cd16be8b9
kyunghoon-kr <byul2sol@naver.com>
2021-02-10 15:49:16 +0900

    Update Baekjoon/김경훈/Q1000.js: change into fs module

--- Baekjoon/김경훈/Q1000.js
+++ Baekjoon/김경훈/Q1000.js
@@ -1,13 +1,2 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let input = [];
-rl.on('line', (line) => {
-    input = line.split(' ').map((el) => parseInt(el));
-})
-.on('close', () => {
-    console.log(`${input[0] + input[1]}`);
-});
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map((el) => parseInt(el));
+console.log(`${input[0] + input[1]}`);
