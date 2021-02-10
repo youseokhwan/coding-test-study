4318cd9ef77bde24a02c9de0b8252f6b9cb59bf6
kyunghoon-kr <byul2sol@naver.com>
2021-02-10 15:14:41 +0900

    Submit Baekjoon/김경훈/Q1001.js

---
+++ Baekjoon/김경훈/Q1001.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+rl.on('line', (line) => {
+    input = line.split(' ').map((el) => parseInt(el));
+})
+.on('close', () => {
+    console.log(`${input[0] - input[1]}`);
+});
