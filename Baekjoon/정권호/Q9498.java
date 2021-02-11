00fb74fc0a76f9e598eaa097f46e56399a87101c
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-11 22:59:16 +0900

    Submit Baekjoon/정권호/Q9498.java

---
+++ Baekjoon/정권호/Q9498.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+
+        switch (a / 10) {
+            case 10:
+            case 9:
+                System.out.print("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
+    }
+}
