64a8e6066c93b7b2afe9b54a2ab7db0603c330b3
jeongwoobin <woobin406@naver.com>
2021-02-11 23:31:08 +0900

    Submit BaekJoon/정우빈/Q2438.kt

---
+++ Baekjoon/정우빈/Q2438.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val a = readLine()!!.toInt()
+    for(i in 1..a) {
+        println("*".repeat(i))
+//        for(j in 1..i) {
+//            print("*")
+//        }
+//        println()
+    }
+}
