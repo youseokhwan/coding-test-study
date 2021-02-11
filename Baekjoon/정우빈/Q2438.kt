f1253be956839600fc2bff5915841cf6069ac441
jeongwoobin <woobin406@naver.com>
2021-02-12 00:06:08 +0900

    Update Baekjoon/정우빈/Q2438.kt: 하나의 출력문으로 변경

--- Baekjoon/정우빈/Q2438.kt
+++ Baekjoon/정우빈/Q2438.kt
@@ -1,10 +1,13 @@
 fun main() {
     val a = readLine()!!.toInt()
+    val str = StringBuilder()
+
     for(i in 1..a) {
-        println("*".repeat(i))
+        str.append("*".repeat(i)).append("\n")
 //        for(j in 1..i) {
 //            print("*")
 //        }
 //        println()
     }
+    println(str)
 }
