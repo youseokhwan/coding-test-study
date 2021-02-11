cb3a4e240a679351e2ddccbcbe0515b04ebcdff5
jeongwoobin <woobin406@naver.com>
2021-02-11 23:30:20 +0900

    Submit BaekJoon/정우빈/Q10869.kt

---
+++ Baekjoon/정우빈/Q10869.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val (a, b) = readLine()!!.split(" ")
+    println(a.toInt() + b.toInt())
+    println(a.toInt() - b.toInt())
+    println(a.toInt() * b.toInt())
+    println(a.toInt() / b.toInt())
+    println(a.toInt() % b.toInt())
+}
