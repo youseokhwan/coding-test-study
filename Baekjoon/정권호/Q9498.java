c6bee8cd633b43cfb4c7944ce7967780c241915e
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-13 21:25:36 +0900

    Update Baekjoon/정권호/Q9498.java: 함수 이름 변경
    line 5 Q9498 에서 Main 으로 수정

--- Baekjoon/정권호/Q9498.java
+++ Baekjoon/정권호/Q9498.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q9498 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
