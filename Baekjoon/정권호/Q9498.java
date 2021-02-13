e97380a5ca0f67bbf160bcb9c75b40d61a99660c
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-13 18:05:42 +0900

    Update Baekjoon/정권호/Q9498.java: 컨벤션 수정
    line 13에 print -> println 으로 수정
    swtich case 들여쓰기 수정

--- Baekjoon/정권호/Q9498.java
+++ Baekjoon/정권호/Q9498.java
@@ -1,29 +1,29 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class Q9498 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(reader.readLine());
 
+        int a = Integer.parseInt(reader.readLine());
         switch (a / 10) {
-            case 10:
-            case 9:
-                System.out.print("A");
-                break;
-            case 8:
-                System.out.println("B");
-                break;
-            case 7:
-                System.out.println("C");
-                break;
-            case 6:
-                System.out.println("D");
-                break;
-            default:
-                System.out.println("F");
-                break;
+        case 10:
+        case 9:
+            System.out.println("A");
+            break;
+        case 8:
+            System.out.println("B");
+            break;
+        case 7:
+            System.out.println("C");
+            break;
+        case 6:
+            System.out.println("D");
+            break;
+        default:
+            System.out.println("F");
+            break;
         }
     }
 }
