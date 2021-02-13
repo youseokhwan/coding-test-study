2008d40109a461ffb1b9c685786b2fd088690e6a
kyunghoon-kr <byul2sol@naver.com>
2021-02-13 16:15:52 +0900

    Submit Baekjoon/김경훈/Q9498.js

---
+++ Baekjoon/김경훈/Q9498.js
@@ -0,0 +1,12 @@
+const input = parseInt(require('fs').readFileSync('/dev/stdin'));
+if (input >= 90) {
+    console.log('A');
+} else if (input >= 80) {
+    console.log('B');
+} else if (input >= 70) {
+    console.log('C');
+} else if (input >= 60) {
+    console.log('D');
+} else {
+    console.log('F');
+}
