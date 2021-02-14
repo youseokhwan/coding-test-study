3b49b0b13162ad4bff2f59885757a43a5b3b5e66
jeongwoobin <woobin406@naver.com>
2021-02-14 21:38:46 +0900

    Update Baekjoon/정우빈/Q2438.kt: 반복문을 두개에서 한개로 변경

--- Baekjoon/정우빈/Q2438.kt
+++ Baekjoon/정우빈/Q2438.kt
@@ -1,9 +1,13 @@
 fun main() {
     val a = readLine()!!.toInt()
     val str = StringBuilder()
+    val star = StringBuilder()
 
     for (i in 1..a) {
-        str.append("*".repeat(i)).append("\n")
+        star.append("*")
+        str.append(star).append("\n")
+
+//        str.append("*".repeat(i)).append("\n")
 //        for(j in 1..i) {
 //            print("*")
 //        }
