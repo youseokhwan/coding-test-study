29e4e8530aef2c5752d9111471654bc5e15f789f
jeongwoobin <woobin406@naver.com>
2021-02-14 21:21:26 +0900

    Update Baekjoon/정우빈/Q2741.kt: for문 뒤 공백 추가

--- Baekjoon/정우빈/Q2741.kt
+++ Baekjoon/정우빈/Q2741.kt
@@ -1,7 +1,7 @@
 fun main () {
     val a = readLine()!!.toInt()
     val str = StringBuilder()
-    for(i in 1..a) {
+    for (i in 1..a) {
         str.append(i).append("\n")
     }
     print(str)
