d0583d4810bfd7cfbb13243e1b7a0fafe930ba9e
jeongwoobin <woobin406@naver.com>
2021-02-14 21:24:27 +0900

    Update Baekjoon/정우빈/Q9498.kt: 오퍼레이터 앞뒤 공백 추가

--- Baekjoon/정우빈/Q9498.kt
+++ Baekjoon/정우빈/Q9498.kt
@@ -1,11 +1,11 @@
 fun main () {
     val a = readLine()!!.toInt()
 
-    if(a>=90) print("A");
-    else if(a>=80) print("B");
-    else if (a>=70) print("C");
-    else if(a>=60) print("D");
-    else print("F");
+    if (a>=90) print("A")
+    else if (a >= 80) print("B")
+    else if (a >= 70) print("C")
+    else if (a >= 60) print("D")
+    else print("F")
 
 //    when(a) {
 //        in 90..100 -> println("A")
