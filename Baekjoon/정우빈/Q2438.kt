b9d9352cbedc085bdf6725550a1d690c3130ec51
jeongwoobin <woobin406@naver.com>
2021-02-14 21:18:13 +0900

    Update Baekjoon/정우빈/Q2438.kt: for문 뒤 공백 추가

--- Baekjoon/정우빈/Q2438.kt
+++ Baekjoon/정우빈/Q2438.kt
@@ -2,7 +2,7 @@ fun main() {
     val a = readLine()!!.toInt()
     val str = StringBuilder()
 
-    for(i in 1..a) {
+    for (i in 1..a) {
         str.append("*".repeat(i)).append("\n")
 //        for(j in 1..i) {
 //            print("*")
