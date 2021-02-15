f5cee25103aafe4c3e1928dfed3e30103d9fb2f3
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-15 21:52:33 +0900

    Submit Baekjoon/정권호/Q1919.java

---
+++ Baekjoon/정권호/Q1919.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        char[] cmp1 = reader.readLine().toCharArray();
+        char[] cmp2 = reader.readLine().toCharArray();
+        List<Character> result = new ArrayList<>();
+
+        for (int i = 0; i < cmp1.length; i++) {
+            result.add(cmp1[i]);
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < cmp2.length; i++) {
+            boolean contains = result.contains(cmp2[i]);
+            if (contains) {
+                result.remove((Character) cmp2[i]);
+                continue;
+            }
+            count++;
+        }
+
+        System.out.println(count + result.size());
+    }
+}
