e47ac4cca291645ae7d2b72c6f4ee73cbeb094eb
youseokhwan <tjrghks115@naver.com>
2021-02-16 17:54:07 +0900

    Update Baekjoon/유석환/Q9012.swift: 삼항 연산자 사용하여 가독성 개선

--- Baekjoon/유석환/Q9012.swift
+++ Baekjoon/유석환/Q9012.swift
@@ -9,11 +9,7 @@ outer: for ps in psList {
     var valid = 0
     
     inner: for parenthesis in ps {
-        if (parenthesis == "(") {
-            valid += 1
-        } else {
-            valid -= 1
-        }
+        valid += parenthesis == "(" ? 1 : -1
         
         if (valid < 0) {
             print("NO")
@@ -21,9 +17,5 @@ outer: for ps in psList {
         }
     }
     
-    if (valid == 0) {
-        print("YES")
-    } else {
-        print("NO")
-    }
+    print(valid == 0 ? "YES" : "NO")
 }
