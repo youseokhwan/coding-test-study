020ba04da29b9c63a4378f97dd549d98b02fcab8
kyunghoon-kr <byul2sol@naver.com>
2021-02-16 20:46:47 +0900

    Update Baekjoon/김경훈/Q1316.js: Set으로 변경

--- Baekjoon/김경훈/Q1316.js
+++ Baekjoon/김경훈/Q1316.js
@@ -4,19 +4,19 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let groupWordsCount = input[0];
 
 for(let i = 1; i <= input[0]; i++) {
-    let alreadyExist = [];
+    let alreadyExist = new Set([]);
     let currentString = input[i][0];
-    alreadyExist.push(currentString);
+    alreadyExist.add(currentString);
     for(let j = 1; j < input[i].length; j++) {
         if(input[i][j] != currentString) {
-            if(alreadyExist.includes(input[i][j])) {
+            if(alreadyExist.has(input[i][j])) {
                 groupWordsCount -= 1;
                 break;
             } else {
-                alreadyExist.push(input[i][j]);
+                alreadyExist.add(input[i][j]);
             }
         } 
-        currentString = input[i][j]
+        currentString = input[i][j];
     }
 }
 
