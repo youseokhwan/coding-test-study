bf74423a703d093cce0870ca974ddcdf0267de04
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-16 20:35:01 +0900

    Update Baekjoon/정권호/Q9012.java: Move variables(ps, result, count) into the loop

--- Baekjoon/정권호/Q9012.java
+++ Baekjoon/정권호/Q9012.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,17 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(reader.readLine());
-        char[] ps;
         StringBuilder sb = new StringBuilder();
-        String result;
-        int count = 0;
 
         for (int i = 0; i < num; i++) {
-            ps = reader.readLine().toCharArray();
-            count = 0;
-            result = "YES";
+            char[] ps = reader.readLine().toCharArray();
+            int count = 0;
+            String result = "YES";
             if (ps.length % 2 == 1) {
                 result = "NO";
+                sb.append(result).append("\n");
+                continue;
             }
             for (int j = 0; j < ps.length; j++) {
                 if (ps[j] == '(') {
