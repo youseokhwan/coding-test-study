14b2bbc0abce858d842a642a555829473d20a576
jeongwoobin <woobin406@naver.com>
2021-02-18 12:58:35 +0900

    Update Baekjoon/정우빈/Q1032.kt: 비교 단어가 ? 가 아닐 때에만 new파일이름과 비교하도록 수정

--- Baekjoon/정우빈/Q1032.kt
+++ Baekjoon/정우빈/Q1032.kt
@@ -1,13 +1,15 @@
-fun main () {
+fun main() {
     val count = readLine()!!.toInt()
     val output = StringBuilder(readLine()!!)
     val length = output.length
 
-    for (i in 0 until count-1) {
+    for (i in 0 until count - 1) {
         val strNew = readLine()!!
         for (j in 0 until length) {
-            if (output[j] != strNew[j]) {
-                output[j] = '?'
+            if (output[j] != '?') {
+                if (output[j] != strNew[j]) {
+                    output[j] = '?'
+                }
             }
         }
     }
