23cf0aefc71a7e0c0089ce0adbf0923ab896a6d5
yuyw0712 <yuyw0712@naver.com>
2021-02-19 18:47:42 +0900

    Update Baekjoon/유양우/Q2217.cpp sort를 한 번으로 줄이고 별도 변수 추가없이 N-i사용

--- Baekjoon/유양우/Q2217.cpp
+++ Baekjoon/유양우/Q2217.cpp
@@ -9,8 +9,8 @@ int main() {
 	int N;
 	// 로프가 견딜 수 있는 무게를 담을 vector v선언
 	vector<int> v;
-	// 몇 개의 로프를 써야 가장 최고 중량이 될 지 값을 담는 vector answer 선언
-	vector<int> answer;
+	// 최고 무게를 담을 answer선언
+	int answer = 0;
 	// 로프의 개수 입력 받기
 	cin >> N;
 	// 가장 적게 버틸 수 있는 중량이면 모든 로프들이 그 만큼 씩 버틸 수 있기 때문에 효율이 안좋은 로프부터 계산하여 하나씩 줄여나가는 변수 a 선언
@@ -24,14 +24,11 @@ int main() {
 	// 가장 적게 버틸 수 있는 중량의 로프 부터 계산하기 위해 오름차순 정렬
 	sort(v.begin(), v.end());
 	// N의 개수에 따라 가장 적게 버틸 수 있는 로프의 중량에 맞춰서 다른 로프들도 
-	// 그 만큼만 들 수 있기 때문에 그 중량에 * a(최고 중량을 구해야 하기 때문에 작은 것부터 줄여나간다)로 answer에 저장하고 a--로 하나씩 줄여나간다.
+	// 그 만큼만 들 수 있기 때문에 그 중량에 * a(최고 중량을 구해야 하기 때문에 작은 것부터 줄여나간다)를 answer에 대입 후 answer보다 큰 값이면 대입한다.
 	for (int i = 0; i < N; i++) {
-		answer.push_back(v[i] * a);
-		a--;
+		if (answer < v[i] * (N - i)) answer = v[i] * (N - i);
 	}
-	// answer에 들 수 있는 가장 적은 중량에서 가장 큰 중량까지 들어 있기 때문에 answer를 오름차순 정렬
-	sort(answer.begin(), answer.end());
-	// answer의 인덱스가 N-1일 때 가장 크므로 프린트
-	cout << answer[N - 1];
 
+	// 최고중량 answer Print
+	cout << answer;
 }
