bab6c3154c98e882b5393214fd3dfead26f89e57
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-22 21:21:55 +0900

    Update Baekjoon/정권호/Q11000.java: Replace TreeSet with PriorityQueue
    line 29 중복이 허용되는 Priority Queue 로 교체

--- Baekjoon/정권호/Q11000.java
+++ Baekjoon/정권호/Q11000.java
@@ -26,20 +26,16 @@ public class Main {
             }
         });
 
-        TreeSet<Double> roomEndTime = new TreeSet<>(); // 이진 검색 트리형태로 데이터를 저장하는 자료구조 (객체를 담을 시 바로바로 정렬을 해서 담아줌), 강의가 끝나는 시간을 담아줄 객체
-        roomEndTime.add((double) lecture[0][1]); // 시작시간이 가장 빠른 강의의 끝나는 시간을 먼저 담아주고 시작
+        PriorityQueue<Integer> endTime = new PriorityQueue<>(); // 우선순위를 먼저 결정하고 그 우선순위가 높은 엘리먼트가 먼저 나가는 자료구조 (객체를 담을 시 바로바로 정렬을 해서 담아줌), 강의가 끝나는 시간을 담아줄 객체
+        endTime.add(lecture[0][1]); // 시작시간이 가장 빠른 강의의 끝나는 시간을 먼저 담아주고 시작
 
         for (int i = 1; i < num; i++) { // 그다음으로 시작시간이 빠른 강의시간 부터 반복
-            if (roomEndTime.first() <= lecture[i][0]) { // roomEndTime 에 들어있는 강의가 끝나는 시간중에 가장 빠른 시간보다 현재 강의의 시작시간이 더 느리면 roomEndTime 에서 가장 빠른 시간을 제거
-                roomEndTime.pollFirst();                // -> 사람말로 풀면 현재 lecture 의 시작 시간이 앞에서 있던 강의의 끝나는 시간 뒤임으로 앞의 강의를 빼주는것...
+            if (endTime.peek() <= lecture[i][0]) { // endTime 에 들어있는 강의가 끝나는 시간중에 가장 빠른 시간보다 현재 강의의 시작시간이 더 느리면 endTime 에서 가장 빠른 시간을 제거
+                endTime.poll();                    // -> 사람말로 풀면 현재 lecture 의 시작 시간이 앞에서 있던 강의의 끝나는 시간 뒤임으로 앞의 강의를 빼주는것...
             }
-            boolean isAdded = roomEndTime.add((double) lecture[i][1]); // 현재 강의의 끝나는 시간을 담아준다
-            if (!isAdded) { // TreeSet 은 중복되는 값을 허용하지 않음으로 로직에 영향이 안가는 만큼 값을 수정해서 저장 -> 이부분은 다른 자료구조를 찾아볼것...
-                roomEndTime.add((double) lecture[i][1] - 0.000001 * i);
-            }
+            endTime.add(lecture[i][1]); // 현재 강의의 끝나는 시간을 담아준다
         }
 
         System.out.println(roomEndTime.size()); // 이 객체의 크기가 곧 강의실 수와 같으므로 출력
     }
 }
-
