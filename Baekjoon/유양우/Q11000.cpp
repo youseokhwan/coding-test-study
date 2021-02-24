e6302c12dae7d3d8475dc4a8a251d40741d2c4f4
yuyw0712 <yuyw0712@naver.com>
2021-02-24 19:55:06 +0900

    Update Baekjoon/유양우/Q11000.cpp 쓸모없는 if else 제거

--- Baekjoon/유양우/Q11000.cpp
+++ Baekjoon/유양우/Q11000.cpp
@@ -5,45 +5,32 @@
 #include <queue>
 using namespace std;
 
-// 종료시간 오름차순으로 정렬 비교 cmp
-bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
-	if (a.second == b.second) {
-		return a.first < b.first;
-	}
-	return a.second < b.second;
-}
-
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 	int N; cin >> N;
-	// vector pair 선언 및 초기화
+	// vector pair 선언
 	vector<pair<int, int>> time;
+	// 오름차순을 우선으로 하는 우선순위큐 선언(Head가 항상 작은 값이 오게끔)
 	priority_queue<int, vector<int>, greater<int> > pq;
-
+	// vector pair 값 입력받기
 	for (int i = 0; i < N; i++) {
 		int start; cin >> start;
 		int end; cin >> end;
 		time.push_back(make_pair(start, end));
 	}
-
-	pq.push(3);
-	cout << pq.top();
-	pq.push(4);
-	cout << pq.top();
-	pq.push(2);
-	cout << pq.top();
-	pq.pop();
-	sort(time.begin(), time.end(), cmp);
+	// pair의 first값을 오름차순으로 정렬 -> 최소의 강의실을 사용해야하기 때문에 시작시간을 기준으로 오름차순 정렬
+	sort(time.begin(), time.end());
+	// 첫 번째 끝나는 시간을 우선순위 큐에 enque
 	pq.push(time[0].second);
 	for (int i = 1; i < N; i++) {
+		// 시작시간이 우선순위큐에 있는 값보다 크면 pop 
 		if (pq.top() <= time[i].first) {
 			pq.pop();
-			pq.push(time[i].second);
 		}
-		else {
-			pq.push(time[i].second);
-		}
+		// 후에 그 끝나는 시간을 넣어준다.
+		pq.push(time[i].second);
 	}
+	// 우선순위 큐의 사이즈를 출력한다
 	cout << pq.size();
 }
