7b0d0065d21ce0019df4e7cb7828f6dbb72dc3e5
jeongwoobin <woobin406@naver.com>
2021-03-11 22:45:10 +0900

    Submit BaekJoon/정우빈/Q2751.kt

---
+++ Baekjoon/정우빈/Q2751.kt
@@ -0,0 +1,16 @@
+import kotlin.collections.ArrayList
+
+fun main() {
+    val size = readLine()!!.toInt()
+    val arr = ArrayList<Int>(size)
+    val result = StringBuilder()
+
+    repeat (size) {
+        arr.add(readLine()!!.toInt())
+    }
+
+    arr.sort()
+    arr.forEach { result.append("${it}\n") }
+
+    print(result)
+}
