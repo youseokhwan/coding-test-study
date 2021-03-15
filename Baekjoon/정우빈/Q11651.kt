51b37118971ab31bb87d93a56772e3430042983f
jeongwoobin <woobin406@naver.com>
2021-03-15 23:02:47 +0900

    Submit BaekJoon/정우빈/Q11650, Q11651.kt

---
+++ Baekjoon/정우빈/Q11651.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    val count = br.readLine()!!.toInt()
+    val pointArr = mutableListOf<Pair<Int, Int>>()
+
+    repeat(count) {
+        val pointInfo = br.readLine()!!.trim().split(" ")
+        pointArr.add(Pair(pointInfo[0].toInt(), pointInfo[1].toInt()))
+    }
+
+    pointArr.sortWith(compareBy ({ it.second }, { it.first }))
+    for (point in pointArr) {
+        bw.write("${point.first} ${point.second}\n")
+    }
+    bw.flush()
+    bw.close()
+    br.close()
+}
