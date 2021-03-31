3ea1d3bc2644a19dd45b17b71b3e4c846ac65f1e
jjh5887 <wjdrnjsgh159@naver.com>
2021-03-31 21:36:26 +0900

    Update Baekjoon/정권호/Q1149.java: Add Comments

--- Baekjoon/정권호/Q1149.java
+++ Baekjoon/정권호/Q1149.java
@@ -5,10 +5,11 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // 입력 받기
         int N = Integer.parseInt(reader.readLine());
         int[][] rgb = new int[N][3];
         int[][] dp = new int[N][3];
+
         for (int i = 0; i < N; i++) {
             StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
             int red = Integer.parseInt(tokenizer.nextToken());
@@ -20,11 +21,11 @@ public class Main {
             rgb[i][2] = blue;
         }
 
-        dp[0][0] = rgb[0][0];
+        dp[0][0] = rgb[0][0]; // rgb 각각의 시작값  0: red, 1: green, 2:blue
         dp[0][1] = rgb[0][1];
         dp[0][2] = rgb[0][2];
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < N; i++) { // 직전의 색깔과만 같지만 않으면 됨
             dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
             dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
             dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + rgb[i][2];
